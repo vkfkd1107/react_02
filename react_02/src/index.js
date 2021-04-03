04acdae1ff4456372ee50fa0ecd580a40252e885
jisu <ji_su1107@naver.com>
2021-04-03 13:32:43 +0900

    hello world

--- react_02/src/index.js
+++ react_02/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hello, world!</h1>,
   document.getElementById('root')
 );
 
