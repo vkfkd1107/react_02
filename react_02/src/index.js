962b7eb951820cfda56fa81206d7d1d16ced952a
jisu <ji_su1107@naver.com>
2021-04-03 13:46:02 +0900

    jsx란?

--- react_02/src/index.js
+++ react_02/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+
+const element = <h1>Hello, world!</h1>;
+// 위의 태그 문법은 jsx라하는 javaScript를 확장한 문법이다
+// javaScript의 모든 기능이 포함되어 있다
+
+// jsx는 react element를 생성한다
+
+// react에서는 별도의 파일에 마크업과 로직을 넣어 기술을 인위적으로 분리하는 대신, 둘 다 포함하는 컴포넌트라고 부르는 느슨하게 연결된 유닛으로 관심사를 분리한다
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
 
